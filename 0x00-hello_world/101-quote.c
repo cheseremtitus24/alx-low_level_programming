87c07cad8ead3b81849d876568cd1d2c1314c213
cheseremtitus <cheseremtitus24@gmail.com>
2022-09-08 12:42:34 -0700

    Feature: Added 'write(option,arg,arg) stdout Function
    It is important to note that it is required that you include '#include <unistd.h>' as a header so that the execution does not throw off an error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Programs prints out a string using write
+ * the *str pointer initializes the string to be passed
+ * and then iterates through it to capture the string length
+ * The string length is the passed in as a parameter to the write
+ * function that will print out the string.
+ * Example:
+ * char str[] = "hello";
+ * int strlen = 5;
+ * write(1,str,5);
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *strptr;
+	int strchars = 0;
+
+	for (strptr = str; *strptr != '\0'; *strptr++)
+	{
+		strchars++;
+	}
+	write(1, str, strchars);
+
+	return (0);
+}
