65b7a266ad5e4cf8337c590176e15a4a3e2af285
cheseremtitus <cheseremtitus24@gmail.com>
2022-09-09 12:42:03 -0700

    feature: Modified 101-print_com4.c to work with pair grouping of pair decimals

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: The program prints 1000C3- "1000" -Combination-'2'
+ * numbers from 0-9
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int a, b, c, d = 48;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (a = 48; a <= 57; a++)
+		{
+			/**
+			 *  for combinations is based on order and
+			 *  always the preceding value should always
+			 *  be smaller than the successor 02 12.
+			 *  12 02 violates this rule since the predecessor is bigger than
+			 *  the successor.
+			 */
+
+			for (b = 48; b <= 57; b++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+
+					if ((c + a) < (b + d) )
+					{
+						putchar(c);
+						putchar(a);
+						putchar(' ');
+						putchar(b);
+						putchar(d);
+					if (a == 56 && c == 57 && b == 57 && d == 57)
+					{
+						continue;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+					}
+
+				}
+
+			}
+
+		}
+	}
+	/* Write the Character to stdout */
+	putchar('\n');
+
+	return (0);
+}
