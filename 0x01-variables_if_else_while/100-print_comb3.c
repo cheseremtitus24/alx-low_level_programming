ce7fd72ecaba49b7a53b21e400cd933d12d4d9c3
cheseremtitus <cheseremtitus24@gmail.com>
2022-09-09 10:38:09 -0700

    feature: Implemented a Nested Loop to Loop Through 00 - 99

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: The program prints 10C2- 10-Combination-2 
+ * numbers from 0-9
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int c = 48;
+	int b = 48;
+
+	for (; c <= 57; c++)
+	{
+		for (int a = 48; a <= 57; a++)
+		{
+
+			/**
+			 *  for combinations is based on order and
+			 *  always the preceding value should always
+			 *  be smaller than the successor 01.
+			 *  10 violates this rule since the predecessor is bigger than
+			 *  the successor.
+			 */
+			if (c < a)
+			{
+				putchar(c);
+				putchar(a);
+				putchar(' ');
+
+			}
+
+		}
+	}
+
+
+
+	/* Write the Character to stdout */
+	putchar('\n');
+
+	return (0);
+}
