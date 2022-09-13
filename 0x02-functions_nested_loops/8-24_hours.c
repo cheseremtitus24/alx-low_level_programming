2fb8db40f50a32d190e0306656e2855e327de396
cheseremtitus <cheseremtitus24@gmail.com>
2022-09-12 23:44:56 -0700

    Feature: Added counts to be minutes and seconds

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,10 +26,10 @@ void jack_bauer(void)
 	while (1)
 	{
 		/*print time in HH : MM format*/
-		int hlval = hour % 10; /* Gets the last digit*/
-		int hfval = hour / 10; /* Gets the first digit*/
-		int mlval = minute % 10;
-		int mfval = minute / 10;
+		int hlval = minute % 10; /* Gets the last digit*/
+		int hfval = minute / 10; /* Gets the first digit*/
+		int mlval = second % 10;
+		int mfval = second / 10;
 		/* Convert the integer to its ascii equivalent for printing*/
 		_putchar(asciilookup[hfval]);
 		_putchar(asciilookup[hlval]);
@@ -38,7 +38,7 @@ void jack_bauer(void)
 		_putchar(asciilookup[mlval]);
 		_putchar('\n');
 		/*increase second in intervals of 60 */
-		second += 60;
+		second += 1;
 		/*update hour, minute and second*/
 		if (second == 60)
 		{
@@ -56,8 +56,8 @@ void jack_bauer(void)
 			minute = 0;
 			second = 0;
 		}
-		sleep(60);              /*system sleep for 1 minute*/
-		if (minute == 59 && hour == 23)
+		sleep(1);              /*system sleep for 1 second*/
+		if (minute == 23 && second == 59)
 		{
 			break;
 		}
