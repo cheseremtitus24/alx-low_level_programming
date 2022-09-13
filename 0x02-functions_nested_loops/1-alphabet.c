706ef9c3603541c80f2814833b8d716ab2db2335
cheseremtitus <cheseremtitus24@gmail.com>
2022-09-12 22:06:01 -0700

    Formatting: Betty Styling of File

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,6 @@
 #include "main.h"
 
 /**
-  * main - Entry point of the Program
-  *
-  * Description: Prints the string _putchar and is end delimited by a new line
-  *
-  * Return: 0 always.
-  */
-
-/**
  * print_alphabet - Function that prints a-z
  *
  * Description - Prints lowercase alphabets in ascending order
