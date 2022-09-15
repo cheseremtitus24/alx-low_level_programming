2f3a0392b745a5c29c252cdc8d3e5ab161f6a4aa
cheseremtitus <cheseremtitus24@gmail.com>
2022-09-15 03:07:44 -0700

    new file:   main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#endif /* _MAIN_H_ */
