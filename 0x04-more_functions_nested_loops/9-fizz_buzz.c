31e5e1a2c547400b90809cf81c491e36534f9ee6
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-15 20:30:31 +0000

    Fix: Changed order of conditional statements

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,20 @@ int main(void)
 
 	for (i = 1; i <= size; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			i == size ? printf("FizzBuzz\n") : printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
 		{
 			i == size ? printf("Fizz\n") : printf("Fizz ");
-			continue;
 		}
 		else if (i % 5 == 0)
 		{
 			i == size ? printf("Buzz\n") : printf("Buzz ");
-			continue;
 		}
-		else if (i % 5 == 0 && i % 3 == 0)
-		{
-			i == size ? printf("FizzBuzz\n") : printf("FizzBuzz ");
-			continue;
-		}
-		if (i == 1)
-			printf(" ");
-		i == size ? printf("%d\n", i) : printf("%d ", i);
-
+		else
+			i == size ? printf("%d\n", i) : printf("%d ", i);
 
 	}
 return (0);
