dd2c895c1a4737d9a249243d75add07f40422415
cheseremtitus24 <cheseremtitus24@gmail.com>
2022-09-16 22:25:11 +0300

    Reformatting: Compliance to Betty Style

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,22 @@
  */
 void print_number(int n)
 {
-  int temp;
-  
-  if (n < 0)
-  {
-    temp = -n;
-    _putchar('-');
-  }
-  else
-  {
-    temp = n;
-  }
-  
-  if (temp / 10) /* Captures the first digit */
-  {
-    print_number(temp / 10); /* Recursively call function to print e.g 1024 - 102 - 10 - 1*/
-  }
-  _putchar((temp % 10) + '0'); /* grabs the first digit of a number */
+	int temp;
+
+	if (n < 0)
+	{
+		temp = -n;
+		_putchar('-');
+	}
+	else
+	{
+		temp = n;
+	}
+	if (temp / 10) /* Captures the first digit */
+	{
+		print_number(temp / 10);
+		/* Recursively call function to print e.g 1024 - 102 - 10 - 1*/
+	}
+	_putchar((temp % 10) + '0');
+	/* grabs the first digit of a number */
 }
