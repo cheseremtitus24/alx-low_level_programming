dcf67239a7a75c15f26509cca3bff16e74f1629e
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-16 02:53:53 +0000

    new file:   10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle 
+ * @n: number of times to print the triangle
+ *
+ * Description - prints the left sided diagonal 
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int l;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < n; i++)
+	{
+		for (l = i; l < n; l++)
+		{
+			_putchar(' ');
+		}
+		for (l = 1; l <= i; l++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
