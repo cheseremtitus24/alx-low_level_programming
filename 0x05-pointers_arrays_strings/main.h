9010c4af4e551f6cf40b16498296b990143d2274
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-20 18:20:29 +0000

    Feature: Utilized use of strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,6 +15,7 @@ int string_length(char *pointer);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 /*
  * Structs, enums and unions definitions
  * Typedefs
