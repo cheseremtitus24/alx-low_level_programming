5b09ba85b0e8cfbda7c2abae9d1e7ec819bfd29b
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-20 16:31:30 +0000

    modified:   8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 /**
- * puts_half - prints items in an array
+ * print_array - prints items in an array
  * @a: input array
  * @n: number of array elements to print out
  *
@@ -13,6 +14,11 @@
 void print_array(int *a, int n)
 {
 	int i;
+
+	if (n < 0)
+	{
+		exit(0);
+	}
 
 	/*1 place less for the '\0' string char*/
 	for (i = 0; i < n; i++)
