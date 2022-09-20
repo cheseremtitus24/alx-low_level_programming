95d979fb9304f9a12645ae51d56a9aea1ab61eba
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-20 15:04:21 +0000

    Feature: Print String characters at Intervals of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - Outputs a string
+ * @str: string to print
+ *
+ * Description- Outputs a string to stdout skipping at intervals
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	unsigned long int i;
+
+	if (strlen(str) < 2)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= strlen(str) - 1; i++)
+		{
+			if (i == 0)
+			{
+				_putchar(str[i]);
+			}
+			else if (i % 2 == 0)
+			{
+				_putchar(str[i]);
+			}
+			else
+				continue;
+
+		}
+		_putchar('\n');
+	}
+}
