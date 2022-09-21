b063754fd14bf5eb1947325528746f35f9a74cf5
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-21 06:41:30 +0000

    new file:   main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <string.h>
+char *_strcat(char *dest, char *src);
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#endif /* _MAIN_H_ */
