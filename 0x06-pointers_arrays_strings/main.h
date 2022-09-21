970bb9f835978a68f91f48cc2dc0a6f174d6cfc4
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-21 20:26:53 +0000

    Feature: Buffer Overrun Magic Example

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,10 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /*
  * Structs, enums and unions definitions
  * Typedefs
