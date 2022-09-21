1b078395bcb019faa2492abbbc3b13e4215f1d1b
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-21 08:37:57 +0000

    Feature: strn{cat,cpy} str{cpy} Implementations

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 #define _MAIN_H_
 #include <string.h>
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 /*
  * Structs, enums and unions definitions
  * Typedefs
