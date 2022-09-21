c31a16f2f7217d936d08c49640e76eccc16de8a1
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-21 16:56:46 +0000

    new file:   3-strcmp.c 	new file:   4-rev_array.c 	new file:   5-string_toupper.c 	new file:   6-cap_string.c 	modified:   main.h

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper- Translates Lowercase to Upper
+ * @s: Input String
+ *
+ * Description- Translates lowercase characters to equivalent Uppercase values.
+ *
+ * Return: string pointer
+ */
+
+char *string_toupper(char *s)
+{
+	int slen = strlen(s);
+	int i, j;
+	char *caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *lower = "abcdefghijklmnopqrstuvwxyz";
+
+	/* iterate through entire input string */
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < strlen(caps); j++)
+		{
+			if (s[i] == lower[j])
+			{
+				s[i] = caps[j];
+			}
+		}
+	}
+
+	return (s);
+}
+
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string- Translates Lowercase to Uppercase
+ * @s: Input String
+ *
+ * Description- Translates lowercase characters to equivalent Uppercase
+ * This is done for every letter succeeding a punctuation character.
+ *
+ * Return: string pointer
+ */
+char *cap_string(char *s)
+{
+	int slen = strlen(s);
+	int i, j, k;
+	char *caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *lower = "abcdefghijklmnopqrstuvwxyz";
+	/*char *punct = ",;.!?\"(){}' ";*/
+	char *punct = " \t\n.?";
+
+	/* iterate through entire input string */
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < strlen(caps); j++)
+		{
+			if (s[i] == lower[j] && i == 0)
+			{
+				s[i] = caps[j];
+				continue;
+			}
+			for (k = 0; punct[k] != '\0'; k++)
+			{
+				if (s[i] == lower[j] && s[i - 1] == punct[k])
+					s[i] = caps[j];
+			}
+
+		}
+	}
+
+	return (s);
+}
+
