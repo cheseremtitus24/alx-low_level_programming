7deeb3a67221a4ee30e0e4d505f509474514e3b8
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-09-24 22:37:04 +0300

    Feature: Added Function to Print a String in Reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <string.h>
 /**
  * _print_rev_recursion- prints a string in reverse
@@ -11,12 +12,30 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		s = s + 1;
-		_print_rev_recursion(s);
-		_putchar(*s);
-	}
+	reverse(s, 0);
 
 }
 
+/**
+ * reverse- prints a string in reverse
+ * @str: string to print in reverse
+ * @k: String index
+ *
+ * Description- outputs a string in reverse using recursion
+ *
+ * Return: void
+ */
+void reverse(char *str, int k)
+{
+	/* base case: end of the string is reached*/
+	if (*(str + k) == '\0')
+	{
+		return;
+	}
+
+	/* recur for the next character*/
+	reverse(str, k + 1);
+
+	/* print current character*/
+	printf("%c", str[k]);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+void reverse(char *str, int k);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
