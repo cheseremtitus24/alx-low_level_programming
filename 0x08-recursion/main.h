785f326cae8eca048f3552a5049e64c5bdc71546
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-09-24 21:21:12 +0300

    modified:   main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,14 @@
 #include <string.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int isPrime(int i, int num);
+
 /*
  * Structs, enums and unions definitions
  * Typedefs
