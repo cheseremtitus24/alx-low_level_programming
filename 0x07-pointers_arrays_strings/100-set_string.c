60f9f5972e202cb0d6a677cbea3f19fbe3692650
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-26 19:45:12 +0000

    Feature: Double pointer to String.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_string- set string pointer
+ * @s: string pointer pointer
+ * @to: string to point to
+ *
+ * Description- prints chessboard
+ * Return: string pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
