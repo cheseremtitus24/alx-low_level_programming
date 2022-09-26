08e1c8a4b15af1993440f16baba152a908584497
Cheserem Titus <cheseremtitus24@gmail.com>
2022-09-26 10:44:00 +0000

    Feature: String Memory Function memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <string.h>
+/**
+ * _memset- appends two string
+ * @s: buffer
+ * @b: bytes to use to fill memory space
+ * @n: number of bytes memory space to fill
+ *
+ * Description- fills first n-indexes of an input buffer- s with the b-bytes.
+ *
+ * Return: character pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/*memset(s, b, n);*/
+
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+		s[i] = b;
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <string.h>
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#endif /* _MAIN_H_ */
