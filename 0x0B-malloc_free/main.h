7a49a8517f94b66a3426dbb7dae274ea4fd25ff7
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-03 15:52:00 +0300

    new file:   0-create_array.c 	new file:   _putchar.c 	new file:   main.h

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,25 @@
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <string.h>
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+void reverse(char *str, int k);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt(int n, int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int isPrime(int i, int num);
+int is_palindrome(char *s);
+int checkPalindrome(char *str, int f, int b, int len);
+int wildcmp(char *s1, char *s2);
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#endif /* _MAIN_H_ */
