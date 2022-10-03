d6cf7d58e46e1d1f295febb2fc9f46d3b60cfc1a
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-03 18:51:48 +0000

    Feature: Handles 0 Sized Array By Returning NULL

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	int i;
 	char *dynmemptr;
 
-	if (size <=0)
+	if (size <= 0)
 		return (NULL);
 	dynmemptr = (char *) malloc(sizeof(char) *  size);
 	if (dynmemptr == NULL)
