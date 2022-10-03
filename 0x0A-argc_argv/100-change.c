4fb606c888b9530cc36ce5da100885fc8b5784fd
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-03 20:27:13 +0000

    Refactoring: Made use of Low Level Calls to Write in printing program output

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -52,6 +52,17 @@ int main(int argc, char **argv)
 
 
 }
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * _atoi- converts an string integer to an integer value
