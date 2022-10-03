9eb3474df61e8e56a7766e9f90675060dd4eeee8
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-03 19:11:41 +0000

    new file:   4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * free_grid- Deallocates Dynamically allocated Memory
+ * @grid: 2D array to Deallocate Dynamic Memory
+ * @height: Number of 1D dynamically Allocated memory Arrays
+ *
+ * Description- Frees the Memory Allocated for a 2D Array
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height);
+{
+	int h;
+
+	if (grid == NULL || height == 0)
+		return;
+	h = 0;
+	while (h < height)
+	{
+		free(grid[h]);
+		h++;
+	}
+	free(grid);
+}
+
