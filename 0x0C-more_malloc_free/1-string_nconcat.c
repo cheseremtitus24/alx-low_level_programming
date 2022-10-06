f7ed2fbf6454fad460d46c72ba7ec9a4734d9583
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-06 18:38:02 +0000

    modified:   1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -67,6 +67,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0 ; i < n && src[i] != '\0' && n <= src_len; i++)
 	{
 		dest[dest_len + i] = src[i];
+		n++;
 	}
 
 	dest[dest_len + i] = '\0';
