3e485af8ec23251320f53fd252cb142e8d560060
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-06 17:15:04 +0000

    new file:   main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <string.h>
+#include <stdlib.h>
+int _putchar(char c);
+int _strlen_recursion(char *s);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
+int _strlen_recursion(char *s);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#endif /* _MAIN_H_ */
