a75329b599f676ca0bc006e24aad6ba04e0df91d
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-06 17:05:53 +0000

    Feature: Created A Heap Array Initialized with Integer Values

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * array_range- Creates an initialized integer array in heap
+ * @min: Start Integer Inclusive
+ * @max: End Integer Inclusive
+ *
+ * Description- Creates an integer array that is integer initialized
+ *
+ * Return: integer pointer
+ */
+int *array_range(int min, int max)
+{
+	int *dynmemptr;
+	int totalsize, i;
+	int initializer;
+
+	/* when nmemb or size is 0 return NULL*/
+	if (min > max)
+		return (NULL);
+
+	/* initiliaze start integer to min*/
+	initializer = min;
+
+	/* create memory store in heap*/
+	totalsize = max - min;
+	dynmemptr = malloc(sizeof(dynmemptr) * totalsize);
+
+	/* Check that memory allocation was a success*/
+	if (dynmemptr == NULL)
+		return (NULL);
+
+	/* initialize allocated memory with zeros*/
+	for (i = 0; i <= totalsize; i++)
+		dynmemptr[i] = initializer++;
+
+	return (dynmemptr);
+}
