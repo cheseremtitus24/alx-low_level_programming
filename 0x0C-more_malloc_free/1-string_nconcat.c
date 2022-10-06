3a647798533ea686def5dd4fc7a68e4133ddf615
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-06 17:28:45 +0000

    Feature: Concatenates Two String in Dynamic Heap Memory

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+ * string_nconcat- Concatenates two strings
+ * @s1: destination string
+ * @s2: source string
+ * @n: length to copy from src to destination
+ *
+ * Description- Appends string contents of src to dest for a specified length.
+ *
+ * Return: char pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *dynmemptr;
+	int i, dest_len;
+
+	/* handle NULL Strings*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	dest_len = _strlen_recursion(s1);
+
+	/* create memory store in heap*/
+	dynmemptr = malloc(dest_len + 1);
+	/* Check that memory allocation was a success*/
+	if (dynmemptr == NULL)
+		return (NULL);
+
+	/* copy the contents of destination to malloc ptr*/
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		dynmemptr[i] = s1[i];
+	}
+	/* call _strncat function*/
+	dynmemptr = _strncat(dynmemptr, s2, n);
+
+	return (dynmemptr);
+}
+/**
+ * _strncat- appends two string
+ * @dest: destination string
+ * @src: source string
+ * @n: length to copy from src to destination
+ *
+ * Description- Appends string contents of src to dest for a specified length.
+ *
+ * Return: character pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dest_len;
+
+	dest_len = _strlen_recursion(dest);
+
+	for (i = 0 ; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen_recursion- prints length of a string
+ * @s: input string
+ *
+ * Description- outputs the length of an input string
+ *
+ * Return: positive integer else 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
