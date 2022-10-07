3e631be14f2223f3f454665e1f609f5f31b94d10
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-07 06:31:17 +0000

    Redo: With String Literal

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,92 +1,51 @@
 #include "main.h"
+#include <string.h>
 /**
  * string_nconcat- Concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: length to copy from src to destination
- *
- * Description- Appends string contents of src to dest for a specified length.
- *
- * Return: char pointer
- */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *dynmemptr;
-	int i, dest_len, src_len;
-
-	/* handle NULL Strings*/
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	dest_len = _strlen_recursion(s1);
-	src_len = _strlen_recursion(s2) + 1;
-
-	/* create memory store in heap*/
-	dynmemptr = malloc(dest_len + 1);
-	/* Check that memory allocation was a success*/
-	if (dynmemptr == NULL)
-		return (NULL);
-
-	/* copy the contents of destination to malloc ptr*/
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		dynmemptr[i] = s1[i];
-	}
-	dynmemptr[i] = '\0';
-
-	/* call _strncat function*/
-	if ((int) n >= src_len)
-	{
-		dynmemptr = _strncat(dynmemptr, s2, src_len);
-	}
-	else
-		dynmemptr = _strncat(dynmemptr, s2, n);
-
-	return (dynmemptr);
-}
-/**
- * _strncat- appends two string
  * @dest: destination string
  * @src: source string
  * @n: length to copy from src to destination
  *
  * Description- Appends string contents of src to dest for a specified length.
  *
- * Return: character pointer
+ * Return: char pointer
  */
-char *_strncat(char *dest, char *src, int n)
+char *string_nconcat(char *dest, char *src, unsigned int n)
 {
 	int i, dest_len, src_len;
+	char *dynmemptr;
 
-	dest_len = _strlen_recursion(dest);
-	src_len = _strlen_recursion(src) + 1;
+	dest_len = strlen(dest) + 1;
+	src_len = strlen(src) + 1;
 
-	for (i = 0 ; i < n && src[i] != '\0' && n <= src_len; i++)
+	/* create memory store in heap*/
+	dynmemptr = malloc(dest_len);
+	/* Check that memory allocation was a success*/
+	if (dynmemptr == NULL)
+		return (NULL);
+
+	/* Copy contents of dest to dynamic memory */
+	for (i = 0; dest[i] != '\0'; i++)
+		dynmemptr[i] = dest[i];
+	dynmemptr[i] = '\0';
+
+	if ((int)n >= src_len)
 	{
-		dest[dest_len - 1 + i] = src[i];
-		n++;
+		/*overwriting the terminating null byte '\0'@ end of dest.*/
+		for (i = 0; src[i] != '\0'; i++)
+			dynmemptr[dest_len + i] = src[i];
+		/* adds a terminating null byte*/
+		dynmemptr[i] = '\0';
+	}
+	else
+	{
+		for (i = 0; i < (int)n && src[i] != '\0'; i++)
+		{
+			dynmemptr[dest_len - 1 + i] = src[i];
+		}
+		dynmemptr[dest_len + i] = '\0';
 	}
 
-	dest[dest_len + i] = '\0';
-	return (dest);
-}
-
-/**
- * _strlen_recursion- prints length of a string
- * @s: input string
- *
- * Description- outputs the length of an input string
- *
- * Return: positive integer else 0
- */
-
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	return (dynmemptr);
 }
 
