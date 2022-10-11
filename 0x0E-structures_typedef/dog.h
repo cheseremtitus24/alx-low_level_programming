b8cfb7162c1985228a969acc124b1574aea15969
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-10-11 16:40:15 +0300

    Simple: Basic Multimember C-Structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <string.h>
+
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+/**
+ * struct dog- Holds details pertaining to a dog
+ * @name: Identifying name
+ * @age: Age of member
+ * @owner: Name of member owner
+ *
+ * Description: This structure holds identifying details of a pet dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif /* _MAIN_H_ */
