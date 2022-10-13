29dc15e56eb5c4d29e2c82563ed777e6fba34791
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-13 18:53:01 +0000

    modified:   3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,160 +14,40 @@ int main(int argc, char **argv)
 	int result, (*op)(int, int);
 
 	result = 0;
-	/* Checks passed in arguments */
 	if (argc == 4)
 	{
 		/* Operator check */
 		if (strcmp(argv[2], "+") == 0 || strcmp(argv[2], "-") == 0 ||
 		strcmp(argv[2], "*") == 0)
 		{
-			/* doing nothing */
 		}
 		else if (strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
 		{
-			if (_atoi(argv[3]) > 0)
+			if (atoi(argv[3]) > 0)
 			{
 			}
 			/* Divide/modulo by zero*/
 			else
 			{
-				_puts_recursion("Error");
+				printf("Error\n");
 				return (100);
 			}
 		}
 				/* Invalid operator provided*/
 		else
 		{
-			_puts_recursion("Error");
+			printf("Error\n");
 			return (99);
 		}
 	}
-	/* Invalid number of commandline arguments provided */
 	else
 	{
-		_puts_recursion("Error");
+		printf("Error\n");
 		return (98);
 	}
 	/* Evaluating Expression */
-	/* function to process the results */
 	op = get_op_func(argv[2]);
-	result = op(_atoi(argv[1]), _atoi(argv[3]));
-	print_number(result);
-	_putchar('\n');
-
+	result = op(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
 	return (0);
-}
-
-/**
- * _atoi- converts an string integer to an integer value
- * @s: string to parse for integer strings
- *
- * Description- Parses for integers within a string and returns an integer
- *
- * Return: integer value else 0 on error
- */
-int _atoi(char *s)
-{
-	int number, i;
-	int numexists;
-	int flag = 0;
-
-	number = 0;
-	numexists = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '-')
-		{
-			flag += 1;
-			continue;
-		}
-		else if ((s[i] >= ' ' && s[i] <= '/') || (s[i] >= ':' && s[i] <= '@'))
-		{
-			/* checks "23 33" returns only 23*/
-			if (s[i - 1] > '0' && s[i - 1] < '9')
-				break;
-			continue;
-		}
-		else if ((s[i] >= '[' && s[i] <= '`') || (s[i] <= 'z' &&
-					s[i] >= 'a') ||  (s[i] <= 'Z' &&
-					s[i] >= 'A') || (s[i] >= '{' && s[i] <= '~')
-				|| s[i] == '\n')
-		{
-			/* checks "23z33" returns only 23*/
-			if (s[i - 1] > '0' && s[i - 1] < '9')
-				break;
-			continue;
-		}
-		numexists += 1;
-		number = number * 10 + s[i] - '0';
-	}
-	if (flag % 2 == 0)
-		return (number);
-	if (numexists < 1)
-		return (0);
-	return (number * -1);
-
-}
-
-/**
- * _puts_recursion- prints a string
- * @s: string to print
- *
- * Description- outputs a string using recursive calls
- * to function _puts_recursion(s)
- *
- * Return: void
- */
-
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s = s + 1;
-	_puts_recursion(s);
-
-}
-
-
-/**
- * print_number - Prints an integer as a char array using recursion
- * @n: Integer to print
- * Return - void
- */
-void print_number(int n)
-{
-	int temp;
-
-	if (n < 0)
-	{
-		temp = -n;
-		_putchar('-');
-	}
-	else
-	{
-		temp = n;
-	}
-	if (temp / 10) /* Captures the first digit */
-	{
-		print_number(temp / 10);
-		/* Recursively call function to print e.g 1024 - 102 - 10 - 1*/
-	}
-	_putchar((temp % 10) + '0');
-	/* grabs the first digit of a number */
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
