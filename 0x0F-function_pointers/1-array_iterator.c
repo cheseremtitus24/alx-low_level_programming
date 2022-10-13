b231ce2691e8b0b72b8b7a7fe810d9a31546b87c
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-10-13 16:56:27 +0300

    new file:   0-print_name.c 	new file:   1-array_iterator.c 	new file:   2-int_index.c 	new file:   README.md 	new file:   function_pointers.h 	deleted:    ../0x0F-function-pointers/0-print_name.c 	deleted:    ../0x0F-function-pointers/1-array_iterator.c 	deleted:    ../0x0F-function-pointers/2-int_index.c 	deleted:    ../0x0F-function-pointers/README.md 	deleted:    ../0x0F-function-pointers/function_pointers.h 	deleted:    ../0x0F_function-pointers/0-print_name.c 	deleted:    ../0x0F_function-pointers/1-array_iterator.c 	deleted:    ../0x0F_function-pointers/2-int_index.c 	deleted:    ../0x0F_function-pointers/README.md 	deleted:    ../0x0F_function-pointers/function_pointers.h

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator- prints formatted integer values
+ * @array: integer array
+ * @size: size of integer array
+ * @action: pointer to a function
+ *
+ * Description: prints interger values from an array through a function pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+
+}
+
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+/**
+ * int_index- Searches for the index of a number in an array
+ * @array: integer array
+ * @size: number of elements in array
+ * @cmp: pointer to a function that compares values
+ *
+ * Description: outputs index of an integer
+ *
+ * Return: an index of the first element for which the cmp function does not
+ * return 0 else return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int result_index = -1;
+
+	/* invoke function on each and every element in array until condition is met*/
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+		{
+			/* Result Found @ index i*/
+			/* break from loop */
+			result_index = i;
+			break;
+		}
+	}
+	/* handles case when not element matches */
+	return (result_index);
+
+}
+
