b77d7d3c3e7b00d9cf819860b6ad40a895d20f02
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-15 01:23:48 +0000

    new file:   3-print_all.c 	modified:   variadic_functions.h

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,74 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+/**
+ * print_all- prints formatted variable types
+ * @format: idata type format specifier
+ *
+ * Description - prints format separated string values
+ * Return: void
+ */
+void print_all(const char *format, ...)
+{
+	va_list ap;
+	char c;
+	float f;
+	int d;
+	int count;
+	char *sp;
+	char *s;
+	char *sep;
+
+	sep = ", ";
+	count = 0;
+
+	va_start(ap, format);
+
+	while (*format)
+	{
+		if (count == 0)
+			sep = "";
+		if (count > 0)
+			sep = ", ";
+		count++;
+		switch (*format++)
+		{
+			case 's':              /* string */
+				sp = va_arg(ap, char *);
+				/*checks and handles NULL srings*/
+				s = sp ? sp : "(nil)";
+				printf("%s%s", sep, s);
+				break;
+
+			case 'i':              /* int */
+				d = va_arg(ap, int);
+				printf("%s%d", sep, d);
+				break;
+
+			case 'f':
+		  		/**
+				 * float  needs a cast here since va_arg only
+				 * takes fully promoted types 
+				 */
+				f = (float) va_arg(ap, double);
+				printf("%s%f", sep, f);
+
+			case 'c':
+		  		/**
+				 * char needs a cast here since va_arg only
+				 * takes fully promoted types 
+				 */
+				c = (char) va_arg(ap, int);
+				printf("%s%c", sep, c);
+				break;
+
+			default:
+				break;
+		}
+	}
+	printf("\n");
+
+	va_end(ap);
+}
