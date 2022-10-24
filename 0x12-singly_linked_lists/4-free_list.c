7663639854a9ffae4a4ed2c967953a6e2b8a3d85
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-10-24 17:35:55 +0300

    new file:   4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list- free dynamically allocated memory of a list node elements
+ * @head: pointer to head of a Linked List 
+ *
+ * Return: void
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+
+}
