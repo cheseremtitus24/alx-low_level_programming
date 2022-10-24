55eeaf3b6b93624100e6893edcea274f659732c8
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-10-24 17:30:02 +0300

    Feature: Function add_node_end Appends Nodes to End of a Singly Linked List

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,115 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node_end- adds nodes to end of linked list
+ * @head: a pointer to a linked list head
+ * @str: initializer node element string
+ * Description: Attaches nodes to the beginning of Linked list
+ * and initializes the len of list to the length of the string
+ *
+ * Return: address of new  nodes else NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int slen, *ptr;
+	char *temp;
+	list_t *new, *lptr;
+
+	slen = 0;
+	ptr = &slen;
+	temp = _strdup(str, ptr);
+
+	/* if temp is null means it failed to allocate heap memory*/
+	if (!temp)
+		return (NULL);
+
+	/* else assign values to the a new node */
+	new = malloc(sizeof(list_t));
+	/**
+	 * check to ensure that malloc allocation of memory is
+	 * successful
+	 */
+	if (!new)
+		return (NULL);
+
+	new->str = temp;
+	new->len = slen;
+	new->next = NULL;
+
+	lptr = *head;
+	if (lptr != NULL)/*handle if list is not empty*/
+	{
+		/*traverse to last element*/
+		while (lptr->next != NULL)
+		{
+			lptr = lptr->next;
+		}
+		/* assign our new item at end of list*/
+		lptr->next = new;
+	}
+	/* if list is empty add new item at the end */
+	else
+		*head = new;
+	return (*head);
+}
+
+/**
+ * _strdup- duplicates a string
+ * @str: String to be duplicated
+ * @ptr: Integer pointer variable to update string length
+ *
+ * Description- Dynamically allocates memory in heap and saves and returns a
+ * copy of the input string str
+ *
+ * Return: char pointer else NULL
+ */
+
+char *_strdup(const char *str, int *ptr)
+{
+	int i;
+	int strsize;
+	char *dynmemptr;
+
+	if (str == NULL)
+		return (NULL);
+	strsize = _sizeof(str);
+
+	dynmemptr = (char *) malloc((sizeof(char) * strsize) + 1);
+
+	if (dynmemptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (int)(sizeof(char) * strsize) ; i++)
+		dynmemptr[i] = *str++;
+	dynmemptr[i + 1] = '\n';
+
+	/* update the pointer to assign length of string*/
+	*ptr = strsize;
+
+	return (dynmemptr);
+
+}
+
+/**
+ * _sizeof- finds the length of a string
+ * @s: String to find the length
+ *
+ * Description- Returns the length of a string less the null pointer '\0'
+ *
+ * Return: positive integer
+ */
+unsigned int _sizeof(const char *s)
+{
+	int i = 0;
+	int count = 0;
+
+	for (; s[i] != '\0'; i++)
+		count++;
+
+	if (count < 1)
+		return (0);
+
+	return (count);
+}
+
+
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -2,3 +2,4 @@ Singly Linked List
 0-print_list.c - prints all the node element values of a linked list
 1-list_len.c - Function counts the number of list nodes in a singly linked list
 2-add_node.c - Adds node items at the beginning of the Singly Linked list
+3-add_node_end.c - Adds nodes to the end of a Singly Linked list
