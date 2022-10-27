8e363464f467eb46ac5b02734d8b62d85d739852
Cheserem Titus <cheseremtitus24@gmail.com>
2022-10-27 17:41:38 +0000

    modified:   9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		/* assign nth node's next this new node*/
 		temp->next = newNode;
 	}
-	return (*head);
+	return (newNode);
 }
 
 /**
