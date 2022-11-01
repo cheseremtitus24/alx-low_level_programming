fb01c30aa74a3f6c6f9fa4a6f89f79c7acc3c571
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-11-01 23:17:33 +0300

    Reformatting: Code Formatting to Conform with Betty Styling

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * get_endianness- checks endianness of a machine /little/big endian 
- *
- *
+ * get_endianness- checks endianness of a machine /little/big endian
  * Return: 0 if big endian else 1
  */
 
 int get_endianness(void)
 {
 	unsigned int num = 1;
-	char *c = (char*) &num;
+	char *c = (char *) &num;
 
-	return (int)*c;
+	return ((int) *c);
 }
