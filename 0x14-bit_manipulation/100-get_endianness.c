98005e9cbedeae9f3fdb111ed6ee61e90e785507
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-11-01 23:14:33 +0300

    Feature: Function that check the Byte Order of Machine Being Either Big/little Endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness- checks endianness of a machine /little/big endian 
+ *
+ *
+ * Return: 0 if big endian else 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *c = (char*) &num;
+
+	return (int)*c;
+}
