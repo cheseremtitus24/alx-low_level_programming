e6bc065655212e4708a2f7336dc02c3624a26bec
Cheserem Titus <cheseremtitus24@gmail.com>
2022-12-08 21:18:56 +0000

    modified:   2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,6 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *new;
 
 	if (head == NULL)
 		return (NULL);
@@ -27,6 +26,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*head)->prev = new_node;
 	*head = new_node;
 	return (new_node);
-};
+}
 
 
