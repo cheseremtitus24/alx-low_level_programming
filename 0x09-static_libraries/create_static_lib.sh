fdc32ed308df631488aae457d34d6579d8ca82eb
Cheserem Titus <50730372+cheseremtitus24@users.noreply.github.com>
2022-12-13 15:31:40 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#c - compile to object files but don't link
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c  *.c 
+# r - replace old symbol references
+# c - create new symbole references library
 ar -rc liball.a *.o
+# update symbol indexes
+ranlib libutil.a
