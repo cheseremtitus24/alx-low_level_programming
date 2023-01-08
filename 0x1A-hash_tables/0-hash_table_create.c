644bca2bcd769367f807a62eec2161898d60dfa6
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-08 12:28:11 +0300

    new file:   0-hash_table_create.c 	new file:   hash_tables.h

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - This function allocates heap memory for a new hash table
+ * @size: Defines the size of the hash map/table
+ *
+ * Description:
+ *
+ * Function creates a hash table by allocating heap memory
+ * space and returning a pointer to the allocated space
+ *
+ * Example:
+ *
+ * Holds a total of 20 items
+ * int array_size = 20;
+ * hash_table_t *new_table = hash_table_create(20);
+ *
+ * Return: a pointer to the heap memory address of the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	/* Create a hash table */
+	hash_table_t *new_hashmap = malloc(size * sizeof(hash_table_t));
+
+	/* Verify that the new hash memory location was successful*/
+	if (new_hashmap == NULL)
+	{
+		/* Memory location failed thus return NULL*/
+		return (NULL);
+	}
+
+	return (new_hashmap);
+
+}
