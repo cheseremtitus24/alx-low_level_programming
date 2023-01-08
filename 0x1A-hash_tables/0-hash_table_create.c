a6d3a8394d1e03f136f17ddfd74ff2a7fd2b9e1b
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-08 14:36:15 +0300

    Feature: Added a handler for case when size is 0 to return exit(-1)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hashmap;
 
 	if (size == 0)
-		return (NULL);
+		exit(EXIT_FAILURE);
 
 	/* Create a hash table */
 	new_hashmap = malloc(size * sizeof(hash_table_t));
