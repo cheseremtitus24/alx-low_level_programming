5e3321dc74cabcb3f45b2db99d9667874ce1369d
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-08 14:30:47 +0300

    modified:   2-key_index.c 	modified:   hash_tables.h

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -14,10 +14,9 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
+
 	/* compute and return the modulus*/
-	if (key)
-		hash = hash_djb2(key);
-	else
-		return (0);
+	hash = hash_djb2(key);
+
 	return (hash % size);
 }
