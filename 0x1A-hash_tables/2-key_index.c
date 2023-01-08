a17a778271cdb58b739f194efbeaacd9c8d48441
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-08 14:27:45 +0300

    new file:   2-key_index.c 	new file:   2-main.c 	modified:   README.md 	modified:   compile.sh 	modified:   hash_tables.h

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * key_index- computes the index of a hash key on the hashmap
+ * @key: The input string that should be hashed to generate a table index
+ * @size: The hash_map size
+ *
+ * Description: Function computes key hash and returns an index that is
+ * within the range from 0-size
+ *
+ * Return: an integer value between 0 and (size - 1)
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+	/* compute and return the modulus*/
+	if (key)
+		hash = hash_djb2(key);
+	else
+		return (0);
+	return (hash % size);
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,1 +1,4 @@
 #c hash tables
+0-hash_table_create.c - Creates a hash table/map
+1-djb2.c - an almost perfect hashing function
+2-key_index.c - a function that maps a hash to an index to a predefined hash_table size
