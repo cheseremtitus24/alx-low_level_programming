98ab81f59bfb0e4be38f91f59969e9655570d550
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-17 12:48:38 +0300

    modified:   0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,12 +20,13 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	/* Verify that the array is not NULL*/
-	if (array == NULL || size == 0)
+	if (!array || size == 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		/* provide stdout status*/
-		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
